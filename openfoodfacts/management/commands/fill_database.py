aa313f536e70fbbdf370f4fe04e03a82a6630a75
Ango <ango.gomes@gmail.com>
2020-08-29 00:30:43 +0200

    Added clean_database and category choice arguments for custom command

--- openfoodfacts/management/commands/fill_database.py
+++ openfoodfacts/management/commands/fill_database.py
@@ -1,6 +1,7 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from pathlib import Path
 from typing import Dict, List
+from django.core.management.base import BaseCommand, CommandParser
+import yaml
 from product.models import Category, Product
 from openfoodfacts.api import Api, DataCleaner
 
@@ -8,25 +9,30 @@ from openfoodfacts.api import Api, DataCleaner
 class FillDatabase:
     """ Class in charge of OpenFoodFacts data recovery and integration in database """
 
-    def __init__(self, categories: List[str]) -> None:
+    def __init__(self, categories: List[str], clean_database) -> None:
         self.api: Api = Api()
         self.clean_datas: DataCleaner = DataCleaner()
         self.categories: List[str] = categories
+        self.clean_database: bool = clean_database
 
     def run(self) -> None:
-        # Product.objects.all().delete()
-        # Category.objects.all().delete()
+        if self.clean_database:
+            Product.objects.all().delete()
+            Category.objects.all().delete()
         for category_name in self.categories:
             result = self.api.get_data(category_name)
             for product in self.clean_datas.get_product(result):
                 self.insert_database(product, category_name)
 
-    def insert_database(self, product_data: Dict, category_name: str) -> None:
+    @staticmethod
+    def insert_database(product_data: Dict, category_name: str) -> None:
         """Method in charge of the database insertion for a product and its relationned category
-        
+
         Args:
-            product_data (Dict): A dictionnary containing all datas to be inserted in database for a product.
-            category (Category): The Products category objects, used to create the relation in database.
+            product_data (Dict): A dictionnary containing all datas
+                to be inserted in database for a product.
+            category (Category): The Products category objects,
+                used to create the relation in database.
         """
 
         product_object, product_created = Product.objects.get_or_create(**product_data)
@@ -39,16 +45,45 @@ class FillDatabase:
 
 
 class Command(BaseCommand):
+    """This class is in charge of the custom command process.
+        Get and add all arguments and run the database insertion process.
+    """
+
     help: str = "Lance la récupération des données de l'API OpenFoodFacts pour remplir la base de données PurBeurre"
+
+    def __init__(self) -> None:
+        self.categories_yaml_path: Path = Path(__file__).parent.parent.parent.joinpath(
+            "categoriesList.yml"
+        )
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "--category",
+            action="append",
+            default=[],
+            dest="categories",
+            help="List of the expected categories products to retrieve from OFF)",
+        )
+
+        parser.add_argument(
+            "--clean",
+            action="store_true",
+            default=False,
+            dest="clean",
+            help="Clean the database before fill it with OFF data",
+        )
 
     def handle(self, *args, **options) -> None:
         print("Lancement de la récupération des données de l'API OFF")
-        categories = [
-            "Sodas",
-            "Desserts",
-            "Sauces",
-        ]
-        fill_database: FillDatabase = FillDatabase(categories)
+
+        with self.categories_yaml_path.open() as yaml_file:
+            data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+        if not options["categories"]:
+            options["categories"] = data["categories"]
+        fill_database: FillDatabase = FillDatabase(
+            options["categories"], options["clean"]
+        )
         fill_database.run()
 
         print("Récupération terminée")
